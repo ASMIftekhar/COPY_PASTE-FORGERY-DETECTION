611f3d6eee83546d16888c0aaef6991884061bd1
ASMIftekhar <iftekhar@ucsb.edu>
2019-12-03 13:59:21 -0800

    new file:   env/bin/python 	new file:   env/bin/python3 	new file:   env/lib64 	new file:   env/pyvenv.cfg 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1WzTNodYUgi6LfV0czs3uDvb4Yr0K9dzQ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1WzTNodYUgi6LfV0czs3uDvb4Yr0K9dzQ" -O pre.zip && rm -rf /tmp/cookies.txt
-
-virtualenv -p /usr/bin/python3.6 copy_move
-source copy_move/bin/activate
-pip3 -r install requirements.txt
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
 
 
 
