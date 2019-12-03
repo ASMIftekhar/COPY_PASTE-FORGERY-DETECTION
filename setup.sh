308f58e85d5c4f2d93532cb5afae8ccbb958ecf1
ASMIftekhar <iftekhar@ucsb.edu>
2019-12-03 14:14:03 -0800

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1WzTNodYUgi6LfV0czs3uDvb4Yr0K9dzQ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1WzTNodYUgi6LfV0czs3uDvb4Yr0K9dzQ" -O pre.zip && rm -rf /tmp/cookies.txt
-python3 -m venv env
-source env/bin/activate
-pip3 install -r requirements.txt
 
-
+unzip pre.zip
 
